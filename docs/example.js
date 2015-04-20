af6aaac758dc43b26af3ebb789d0ef0c644a15b8
Mark Macdonald <mark.t.macdonald@googlemail.com>
2015-04-20 15:28:10 +0200

    add reusable component and jQuery plugin

--- docs/example.js
+++ docs/example.js
@@ -1,7 +1,9 @@
+var view = new View();
+
 function Todo ()
 {
 	var self = this;
-	var view = new View();
+	var widget = new Widget();
 
 	var elements = ['Fish', 'Mangoes', 'Nougat'];
 
@@ -15,11 +17,14 @@ function Todo ()
 
 	this.redraw = function () {
 		var vdom= view.h('div', [
-			list(),
+			view.h('p#plugin'),
+			widget.draw('Example app'), //external component
+			list(), //internal component
 	    	view.h('input#value', { type: 'text' } ),
-			view.h('button#add', { onclick: clickHandler }, 'Add'),
+			view.h('button#add', { onclick: clickHandler }, 'Add')
 		]);
 		view.render(vdom);
+		$('#plugin').foo(); //call jQuery plugin
 	}
 
 	function clickHandler () {
@@ -27,6 +32,29 @@ function Todo ()
 		self.redraw();
 	}
 }
+
+/*
+	A reusable component
+*/
+
+function Widget ()
+{
+	this.draw = function (message) {
+		return view.h('p.w-message.w-information', message);
+	}
+}
+
+/*
+	A jQuery plugin
+*/
+
+(function($) {
+    $.fn.foo = function() {
+        this.each( function() {
+            $(this).text('Example integrated jQuery plugin');
+        });
+    }
+}(jQuery));
 
 $(document).ready( function() {
 	var todo = new Todo();
