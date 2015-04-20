178a002f7fd4d08efca28be369a1662ce322159a
Mark Macdonald <mark.t.macdonald@googlemail.com>
2015-04-20 11:46:05 +0200

    modified example, now uses event handlers.

--- docs/example.js
+++ docs/example.js
@@ -1,32 +1,31 @@
-/*
-  application logic
-*/
-
-var count = 0;
 
 var view = new View();
 
-function render (count) {
-	return view.h('div', { 
-		style: {
-        	textAlign: 'center',
-        	lineHeight: (100) + 'px',
-        	border: '1px solid red',
-        	width: (100) + 'px',
-        	height: (100) + 'px'
-        }
-    }, [String(count)]);
+var elements = ['Fish', 'Mangoes', 'Nougat'];
+
+function renderList () {
+	var items = [];
+	elements.forEach( function (element) {
+    	items.push(view.h('p', element));
+	});
+	return view.h('div', items);
+}
+
+function render () {
+	var vdom= view.h('div', [
+		renderList(),
+    	view.h('input#value', { type: 'text' }),
+		view.h('button#add', 'Add'),
+	]);
+	view.render(vdom);
+}
+
+function clickHandler () {
+	elements.push($('#value').val());
+	render();
 }
 
 $( document ).ready(function() {
-
-	var vdom = render(count);
-	view.render(vdom);
-
-	setInterval(function () {
-	    count++;
-	    var vdom = render(count);
-	    view.render(vdom);
-	}, 1000);
-
+	render();
+	$('#add').on('click', clickHandler);
 });
